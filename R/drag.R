c66e5c7d08756d946afbd0db6d53cf89bed491d3
hadley <h.wickham@gmail.com>
2015-01-07 14:13:43 -0600

    Document tk_drag

--- R/drag.R
+++ R/drag.R
@@ -10,6 +10,18 @@ tangle_deps <- function() {
   )
 }
 
+#' Create a tangle kit slider
+#'
+#' @param value Starting value.
+#' @param text Text shown after the number
+#' @param min,max Minimum and maximum allowable values.
+#' @param id A unique identifier for the control. If unspecified, uses
+#'   lower case value with non-alphanumeric values converted to -,
+#'   multiple - collapsed to one -, and leading and trailing - dropped.
+#' @export
+#' @examples
+#' tk_drag(1, "pies")
+#' tk_drag(2, "hats")
 tk_drag <- function(value, text, min = NULL, max = NULL, id = as_attr(text)) {
   stopifnot(is.numeric(value))
 
