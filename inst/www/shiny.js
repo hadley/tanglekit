802aa1654195a1dcb853985cf977c5f35def9f0d
hadley <h.wickham@gmail.com>
2015-01-08 15:45:47 -0600

    Add debouncer

--- inst/www/shiny.js
+++ inst/www/shiny.js
@@ -1,3 +1,23 @@
+// From http://davidwalsh.name/javascript-debounce-function
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+  var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
 // Each element gets own tangle containing a single variable called with
 // the same name as the id
 var tangles = {};
@@ -23,8 +43,9 @@ $.extend(tangleBinding, {
     tangles[id].setValue(id, value);
   },
   subscribe: function(el, callback) {
+    var db_callback = debounce(callback, 100);
     $(el).on("change.TKAdjustableNumber", function(e) {
-      callback();
+      db_callback();
     });
   },
   unsubscribe: function(el) {
